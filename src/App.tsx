47245780e74ffa0e8d4631b3ca1abebf6ec6faaa
Ivan Egorov <www.Len244@gmail.com>
2022-03-08 22:03:13 +0300

    Some lazy markup. Ill go play FFXIV for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 
 import AuthPage from './components/AuthPage';
 import IndexPage from './components/IndexPage';
 
+const theme = createTheme({
+  palette: {
+  },
+});
+
 export default function App(): JSX.Element {
   return (
-    <Routes>
-      <Route path="/" element={<IndexPage />} />
-      <Route path="/auth" element={<AuthPage />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
--- src/components/AuthPage/index.tsx
+++ src/components/AuthPage/index.tsx
@@ -1,3 +1,31 @@
+import { Box, Button, Container, Paper, TextField } from '@mui/material';
+
 export default function AuthPage(): JSX.Element {
-  return <div>Sheesh</div>;
+  return (
+    <Container
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Paper
+        sx={{
+          width: 'fit-content',
+          padding: 1,
+          gap: 1,
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <TextField placeholder="login" />
+        <TextField placeholder="password" />
+        <Box>
+          <Button>Sign in</Button>
+          <Button>Sign on</Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
